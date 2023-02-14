2aed4144eceef8fbf4fdca96b117c35536e9ce9f
talsaRam <talsaramsutar786@gmail.com>
2023-02-14 06:07:31 +0530

    new

--- forensic/js/custome.js
+++ forensic/js/custome.js
@@ -26,7 +26,7 @@ $('.owl-carousel').owlCarousel({
     loop: true,
     nav: true,
     dots: false,
-    // autoplay: true,
+    autoplay: true,
     autoplaySpeed: 1000,
     smartSpeed: 1500,
     responsive: {
@@ -41,4 +41,128 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+
+
+
+$('.carousel-slider').slick({
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 3,
+    loop: true,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    dots: false,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            arrows: false,
+            dots: true
+        }
+
+    },
+    {
+        breakpoint: 700,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: true,
+            infinite: true,
+            arrows: false
+        }
+    }]
+});
+
+$('.slick-carousel-client').slick({
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 5,
+    loop: true,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    dots: false,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            arrows: false,
+            dots: true
+        }
+    },
+    {
+        breakpoint: 700,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            dots: true,
+            infinite: true,
+            arrows: false
+        }
+    },
+    {
+        breakpoint: 580,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            dots: true,
+            infinite: true,
+            arrows: false
+        }
+    },
+    {
+        breakpoint: 450,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: true,
+            infinite: true,
+            arrows: false
+        }
+    }]
+});
+
+$('.testimonials-slider').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    dots: true,
+    slidesToShow: 3,
+    responsive: [
+        {
+            breakpoint: 1048,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }
+    ]
+});
+
 
